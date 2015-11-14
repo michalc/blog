2f5135ac321816a0a6971b9f7a493389691f4412
Michal Charemza <michalcharemza@gmail.com>
2015-11-14 09:39:12 +0000

    Gulp to run jekyll

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+
+gulp.task('jekyll', function (gulpCallBack){
+    var spawn = require('child_process').spawn;
+    var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+
+    jekyll.on('exit', function(code) {
+        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+    });
+});
+
+
+gulp.task('default', ['jekyll']);
