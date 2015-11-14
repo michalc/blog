2c76f896f28cb8232c273b2159e61c274c2a3365
Michal Charemza <michalcharemza@gmail.com>
2015-11-14 10:05:38 +0000

    Moving to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
 
-gulp.task('jekyll', function (gulpCallBack){
-    var spawn = require('child_process').spawn;
-    var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+gulp.task('jekyll', function(gulpCallBack) {
+  var spawn = require('child_process').spawn;
+  var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
 
-    jekyll.on('exit', function(code) {
-        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
-    });
+  jekyll.on('exit', function(code) {
+    gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+  });
 });
 
+gulp.task('htmlmin', ['jekyll'], function() {
+  var minifyHtml = require('gulp-minify-html');
 
-gulp.task('default', ['jekyll']);
+  return gulp.src('_site/**/*.html')
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('_site'));
+});
+
+gulp.task('html5-lint', ['htmlmin'], function() {
+  var html5Lint = require('gulp-html5-lint');
+
+  return gulp.src('_site/**/*.html')
+    .pipe(html5Lint());
+});
+
+gulp.task('default', ['html5-lint']);
