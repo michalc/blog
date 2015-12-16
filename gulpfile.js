cdfbdd87f4fa7cfa5ab2bc8ed2c6163520583643
Michal Charemza <michalcharemza@gmail.com>
2015-12-16 07:52:42 +0000

    Specify versioned font names only once

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var gulp = require('gulp');
 // Unfortunately can't keep the fonts in this repository, so download them from charemza.name
 gulp.task('fonts', function() {
   var download = require('gulp-download');
+  var rename = require('gulp-regex-rename');
 
   return download([
     'http://charemza.name/assets/proxima-nova/2D48A7_0_0-67e7438dc823bfc16e9b3ef293063daf.woff',
     'http://charemza.name/assets/proxima-nova/2D48A7_1_0-98f78589365b036ffdd1110af0639760.woff',
     'http://charemza.name/assets/proxima-nova/2D48A7_2_0-132aedb0469a775130a3b5d0d3d4f37a.woff'
-  ]).pipe(gulp.dest('src/assets/proxima-nova'));
+  ]).pipe(rename(/(.+)-.+\.woff$/, '$1.woff'))
+  .pipe(gulp.dest('src/_assets/fonts/proxima-nova'));
 });
 
 gulp.task('jekyll', function(gulpCallBack) {
