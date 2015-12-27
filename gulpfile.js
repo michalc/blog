31dddd0d35d7b94ad6a9829568be3d760f4180e8
Michal Charemza <michalcharemza@gmail.com>
2015-12-27 14:28:00 +0000

    Use awspublish instead of s3_website

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var BUILD_DIR = 'build';
 
 // Unfortunately can't keep the fonts in this repository for licensing reasons
 // so download them from charemza.name
@@ -30,9 +31,59 @@ gulp.task('jekyll', ['fonts'], function(gulpCallBack) {
 gulp.task('html5-lint', ['jekyll'], function() {
   var html5Lint = require('gulp-html5-lint');
 
-  return gulp.src('build/**/*.html')
+  return gulp.src('**/*.html', {cwd: BUILD_DIR})
     .pipe(html5Lint());
 });
 
-gulp.task('default', ['html5-lint']);
+gulp.task('build', ['html5-lint']);
+
+gulp.task('publish', [], function() {
+  var awspublish = require('gulp-awspublish');
+  var mergeStream = require('merge-stream');
+  var concurrent = require('concurrent-transform');
+  var publisher = awspublish.create({
+    region: 'eu-west-1',
+    params: {
+      Bucket: 'charemza.name'
+    }
+  });
+
+  // All files are forced since gulp-awspublish doesn't
+  // sync if there are just http header changes
+  function publish(headers) {
+    return concurrent(publisher.publish(headers,{ force:true}), 8);
+  }
+
+  // Cache 5 mins + gzip
+  var html = gulp.src('**/*.html', {cwd: BUILD_DIR})
+    .pipe(awspublish.gzip())
+    .pipe(publish({
+      'Cache-Control': 'max-age=' + 60 * 5 + ', no-transform, public'
+    }));
+
+  // Cache 5 mins, no gzip
+  var meta = gulp.src(['robots.txt', 'sitemap.xml'], {cwd: BUILD_DIR})
+    .pipe(publish({
+      'Cache-Control': 'max-age=' + 60 * 5 + ', no-transform, public'
+    }));
+
+  // Cache 1 week, +  gzip
+  var textResources = gulp.src(['**/*.js', '**/*.css'], {cwd: BUILD_DIR})
+    .pipe(awspublish.gzip())
+    .pipe(publish({
+      'Cache-Control': 'max-age=' + 60 * 60 * 24 * 7 + ', no-transform, public'
+    }));
+
+  // Cache 1 week, no gzip
+  var binaryResources = gulp.src(['**/*.ico', '**/*.woff', '**/*.jpeg'], {cwd: BUILD_DIR})
+    .pipe(publish({
+      'Cache-Control': 'max-age=' + 60 * 60 * 24 * 7 + ', no-transform, public'
+    }));
+
+  return mergeStream(html, meta, textResources, binaryResources)
+    .pipe(publisher.sync())
+    .pipe(awspublish.reporter());
+});
+
+gulp.task('default', ['build']);
 
